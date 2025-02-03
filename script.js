7b0c9a25ab532eb96e1e417f501f77448165df1f
conztantin <conztantinsixten@gmail.com>
2025-02-03 13:33:14 +0100

    console logged all informations

---
+++ script.js
@@ -0,0 +1,13 @@
+let person = {
+    firstname: "John", 
+    lastname: "Johnsen",
+    age: 54,
+    pet: "Cape cobra"
+}
+
+// console.log(person.firstname)
+// console.log(person.lastname);
+// console.log(person["pet"]);
+console.log(person["age"]);
+
+
